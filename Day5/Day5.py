e2448920c61b383efe950b1331ec1a6d8699d95b
EGTECHNOLOGY\aishwaryapattar <aishwaryapattar@egtechnology.co.uk>
2024-12-05 14:26:33 +0000

    refactor of part 1 into functions

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,3 +1,43 @@
+def check_element_safety(page_order_dict, nums, index):
+    # if safe, return j = 0. else return the index it fails at
+    nums_len = len(nums)
+    values = page_order_dict.get(int(nums[index]))
+    j = index + 1
+    while j < nums_len:
+        if (values is None) or (int(nums[j]) not in values):
+            return j
+        j += 1
+    return 0
+
+
+def check_line_safety(nums, start_index):
+    nums_len = len(nums)
+    i = start_index
+
+    while i < nums_len - 1:
+        unsafe_index = check_element_safety(page_order_dict, nums, i)
+        if unsafe_index != 0:
+            return [i, unsafe_index]
+        i += 1
+    
+    return []
+
+def get_middle_number(nums):
+    middle_index = int((len(nums) + 1) / 2)
+    return int(nums[middle_index - 1])
+
+def calculate_correction(line, index1, index2):
+    # swap the incorrect values
+    value1 = line[index1]
+    value2 = line[index2]
+    line[index1] = value2
+    line[index2] = value1
+
+    # check if it is valid
+
+
+    return 0
+
 #######################################################################
 with open('C:\SourceCode\AdventOfCode24\Day5\puzzle_input.txt', 'r') as f:
     lines = f.readlines()
@@ -5,6 +45,8 @@ with open('C:\SourceCode\AdventOfCode24\Day5\puzzle_input.txt', 'r') as f:
 populate_dictionary_complete = False
 page_order_dict = {}
 sum = 0
+corrections = 0
+unsafe_info_list = []
 
 for line in lines:
     if populate_dictionary_complete == False:
@@ -29,29 +71,13 @@ for line in lines:
     else:
         # check which updates are in the correct order
         nums = line.split(',')
-        nums_len = len(nums)
-        safe = True
-
-        for i in range(nums_len - 1):
-            j = i + 1
-            while j < nums_len:
-                values = page_order_dict.get(int(nums[i]))
-                if values is None:
-                    safe = False
-                    break
- 
-                if int(nums[j]) not in values:
-                    safe = False
-                    break
-                j += 1
-            
-            if (safe == False):
-                break
+        unsafe_indices = check_line_safety(nums, 0)
+        safe = len(unsafe_indices) == 0
         
         # if the whole line is safe, find the middle number
         if (safe):
-            middle_index = int((nums_len + 1) / 2)
-            sum += int(nums[middle_index - 1])
-    
+            sum += get_middle_number(nums)
+            
 print("Part 1 = ", sum)
-        
+print("Part 2 = ", corrections)
+    
