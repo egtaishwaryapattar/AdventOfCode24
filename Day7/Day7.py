84ea578be791c2f3c4e8b20432664b5bbf305997
Aishwarya Pattar <aishwaryapattar@Aishwaryas-MacBook-Air.local>
2024-12-07 12:21:11 +0000

    Day 7 part 2 done

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -6,6 +6,8 @@ import os
 class Solution:
     def __init__(self):
         self.equations = {}
+        self.failed_targets = []
+        self.part1_sum = 0
         
 
     def parse_input(self, filename):
@@ -14,30 +16,40 @@ class Solution:
 
         for line in lines:
             parts = line.split(': ')
+            target = int(parts[0])
             values = parts[1].split(' ')
             nums = []
             for value in values:
                 nums.append(int(value))
 
             # not sure if this situation occurs in the puzzle input...
-            if (self.equations.get(int(parts[0])) is not None):
+            if (self.equations.get(target) is not None):
                 raise Exception("Scenario where two equations need to result in the same number")
 
-            self.equations[int(parts[0])] = nums
+            self.equations[target] = nums
 
 
     def part_one(self):
         sum = 0
         for target in self.equations:
-            if self.is_equation_valid(target, self.equations.get(target)):
-                sum += target   
+            if self.is_equation_valid(target, self.equations.get(target), ['+', '*']):
+                sum += target
+            else:
+                self.failed_targets.append(target)
+        self.part1_sum = sum
         return sum
 
 
-    def is_equation_valid(self, target, nums):
+    def part_two(self):
+        sum = 0
+        for target in self.failed_targets:
+            if self.is_equation_valid(target, self.equations.get(target), ['+', '*', '||']):
+                sum += target
+        return self.part1_sum + sum
+    
+
+    def is_equation_valid(self, target, nums, operators):
         len_nums = len(nums)
-        sum_of_nums = sum(nums)
-        product_of_nums = math.prod(nums)
         
         # test if combination of multiplication and addition is needed
         arr = []
@@ -48,10 +60,14 @@ class Solution:
                 temp = arr.copy()
                 arr.clear()
                 for val in temp:
-                    # the array size keeps increasing with each calculation - each time creating two branches - one for addition one for multiplication
-                    arr.append(val + nums[i])
-                    arr.append(val * nums[i])
-
+                    # the array size keeps increasing with each calculation - each time creating - one for each operation
+                    for operator in operators:
+                        if operator == '+':
+                            arr.append(val + nums[i])
+                        elif operator == '*':
+                            arr.append(val * nums[i])
+                        elif operator == '||':
+                            arr.append(int(str(val)+str(nums[i])))
         return target in arr
 
 
@@ -64,5 +80,7 @@ solution.parse_input(filename)
 start = perf_counter()
 answer_part_one = solution.part_one()
 print("Part 1 = ", answer_part_one)
+answer_part_two = solution.part_two()
+print("Part 2 = ", answer_part_two)
 end = perf_counter()
 print(f"Duration = {end - start}")
